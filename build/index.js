67de9143af9e2911f79fffa49aa6f47245e2e5e8
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-05-21 00:08:08 +0600

    some practice

--- build/index.js
+++ build/index.js
@@ -1,3 +1,16 @@
 "use strict";
 let firstName = "shafiqul islam sagor";
 console.log(firstName);
+const json = JSON.parse('55');
+console.log(typeof json);
+let u = true;
+u = false;
+console.log(u);
+let Any = 'shafiqul islam sagor';
+Any = true;
+console.log(Any); // safe holo any
+let unknownNotsafe = 'shafiqul';
+unknownNotsafe = 587;
+console.log(unknownNotsafe);
+const ArrayName = ['s', 'a', 'e'];
+console.log(ArrayName);
