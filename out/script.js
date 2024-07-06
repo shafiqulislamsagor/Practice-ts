bd8ecb2195128f1d2c664bef126f40e085df3f87
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-07-06 07:41:52 +0600

    object type

--- out/script.js
+++ out/script.js
@@ -18,11 +18,36 @@
 // let fruits = ['apple', 'orange', 'banana' ]
 // fruits.push('sagor')
 // console.log(fruits)
-let person = {
-    name: "Masrafi",
-    age: 34,
-    inCaptain: true
-};
-person.age = 30;
+// let person = {
+//     name : "Masrafi",
+//     age: 34,
+//     inCaptain: true
+// }
+// person.age = 30
 // person.country = "bangladesh"  error message
-console.log(person);
+// console.log(person)
+// let a:string
+// let b:number
+// a = 'sagor'
+// b = 34
+// console.log(a , b)
+// string/number/boolean array
+// let a:string[] = []
+// a.push('apple')
+// a.push(5700) error message
+// string and number array
+// let a:(string | number)[] = []
+// a.push('sagor',54 , 74 , 'apple') 
+// object 
+let c;
+// c = {
+//     name:'sagor',
+//     age: 34,
+//     inCaptain: true
+// } error massage 
+c = {
+    name: 'sagor',
+    age: 34,
+    country: 'bangladesh',
+    home: 'dhaka'
+};
--- src/script.ts
+++ src/script.ts
@@ -34,13 +34,54 @@
 
 // console.log(fruits)
 
-let person = {
-    name : "Masrafi",
-    age: 34,
-    inCaptain: true
-}
+// let person = {
+//     name : "Masrafi",
+//     age: 34,
+//     inCaptain: true
+// }
 
-person.age = 30
+// person.age = 30
 // person.country = "bangladesh"  error message
 
-console.log(person)
+// console.log(person)
+
+// let a:string
+
+// let b:number
+
+// a = 'sagor'
+
+// b = 34
+
+// console.log(a , b)
+
+// string/number/boolean array
+
+// let a:string[] = []
+
+// a.push('apple')
+// a.push(5700) error message
+
+// string and number array
+
+// let a:(string | number)[] = []
+
+// a.push('sagor',54 , 74 , 'apple') 
+
+// object 
+
+let c : {name:string , age:number , country:string , home: string} 
+
+// c = {
+//     name:'sagor',
+//     age: 34,
+//     inCaptain: true
+// } error massage 
+
+c = {
+    name:'sagor',
+    age: 34,
+    country: 'bangladesh',
+    home: 'dhaka'
+}
+
