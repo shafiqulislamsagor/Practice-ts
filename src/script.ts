f296fa8a4eb2aca4499c9616e718465ee46fa3ab
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-07-06 07:02:59 +0600

    string , number and array practice

--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,35 @@
-console.log('Starting')
+// console.log('Starting')
 
-const country = "Sherpur"
+// const country = "I live in Sherpur"
 
-console.log(country)
+// console.log(country)
+
+// let playerName  = "masrafi";
+// console.log(playerName)
+
+// playerName = 34 ;
+
+// console.log(playerName)
+
+
+// let playerName ;
+
+// playerName = "masrafi";
+
+// playerName = 21 ;
+
+// playerName = true ;
+
+// console.log(playerName)
+
+// function multiply(a : number,b : any){
+//     return a * b;
+// }
+
+// console.log(multiply(2,3))
+
+let fruits = ['apple', 'orange', 'banana' ]
+
+fruits.push('sagor')
+
+console.log(fruits)
